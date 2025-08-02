09d0448934b04031b232b5ee182d503e99dc2e68
sourabhchalke <sourabhchalke2157@gmail.com>
2025-08-02 11:43:15 +0530

    Form-handling events with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import MyButton from './components/Button';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <MyButton onClick={()=>alert("Button Clicked")} text='Click ME'/>
       {/* <MyButton text='Click ME Again'/> */}
+      <Form/>
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,30 @@
+import { log } from "console";
+import React, { ChangeEventHandler, useState } from "react";
+
+const Form=()=>{
+
+    const [value,setValue]=useState("");
+
+    const changeHandler=(e:React.ChangeEvent<HTMLInputElement>)=>{
+        console.log(e);
+        setValue(e.target.value);
+    }
+
+    const submitHandler=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        console.log(e);
+    }
+
+    return (
+        <div>
+            <form  onSubmit={submitHandler} action="">
+                <input onChange={changeHandler} type="text" placeholder="Enter Your Detail" />
+                <button type="submit">Submit</button>
+            </form>
+            <h2>{value}</h2>
+        </div>
+    )
+
+}
+
+export default Form;
